17fd37349ad514be4a9c545258fd3282d31aa40a
Malik <munirmalik327@gmail.com>
2025-08-22 17:49:42 +0500

    user enter 2 numbers&show result of all operations
    Let the user enter two numbers and show the result of all operations

---
+++ Calculator pratice project1.py
@@ -0,0 +1,15 @@
+
+                            # Basic Math Operations
+
+         # Let the user enter two numbers and show the result of all operations.
+
+
+
+a=float(input("Enter the 1st  value "))
+b=float(input("Enter the 2nd Value "))
+
+print("Addition:", a+b)
+print("Subtraction:", a-b)
+print("Multiplication:", a*b)
+print("Divide", a/b)
+print("Remainder:", a%b)
