b535d5a35380cef558ce1da2ffd2858f9240d640
Malik <munirmalik327@gmail.com>
2025-08-22 17:41:31 +0500

    Area of a Rectangle
    Take width and height from the user and calculate the area

---
+++ Calcuator pratice project2.py
@@ -0,0 +1,11 @@
+                      
+  
+                        #Area of a Rectangle
+          #Take width and height from the user and calculate the area.
+
+width=float(input("Enter the Width"))
+height=float(input("Enter the Height"))
+
+area = (width * height)
+
+print ("area of rectangular:", area )
