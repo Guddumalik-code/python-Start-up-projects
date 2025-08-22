d532e7d8557298200911936b37d621672f3585cb
Malik <munirmalik327@gmail.com>
2025-08-22 17:25:30 +0500

    Add files via upload
    Convert seconds to minutes and hours (using only division and input

---
+++ Seconds to Minutes and Hours Converter.py
@@ -0,0 +1,10 @@
+ 
+                  #Seconds to Minutes and Hours Converter
+         # Convert seconds to minutes and hours (using only division and input)
+
+sec=int(input("Enter Number of Sec :"))
+mint= sec/60
+hours= sec/3600
+
+print("minutes : ",mint)
+print("hours :", hours)
